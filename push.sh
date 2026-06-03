e7dac17670e72517a21440bae9e466340279b24c
bitchikogulara <bichiko2004@gmail.com>
2026-06-03 21:28:18 +0200

    Remove unnecessary push script

--- push.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-git init
-git add .
-git commit -m "Initial commit"
-git branch -M main
-git remote add origin git@github.com:bitchikogulara/ai_project_demonstration_website.git
-git push -u origin main
